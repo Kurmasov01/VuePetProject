52b8163e6c3643a6c1209d9d423bf3df0d958eea
kurmasov01 <kurmasov01@mail.ru>
2024-12-09 23:12:46 +0300

    просто тест

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="main-wrapper">
+    <div class="input-wrapper">
+      <h1>Введите возраст!</h1>
+      <input type="text" value="{{ inputValue }}" v-model="inputValue"/>
+      <button @click="printValue(inputValue)" class="submitBtn">Подтвердить</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data(){
+    return{
+      inputValue: ''
+    };
   },
+  name: "HomeView",
+  components: {},
+  methods: {
+    printValue(inputValue) {
+      alert('Поздравляю вам ' + inputValue + " лет!")
+    }
+  }
 };
 </script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+.main-wrapper {
+  color: white;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  background-color: #6d6d6d;
+}
+.input-wrapper{
+  display: flex;
+  flex-direction: column;
+}
+</style>
